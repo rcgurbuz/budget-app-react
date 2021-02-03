d4944c90669637ad4f9cbb2485d5cd54186f7554
rcgurbuz <rifatcgurbuz@gmail.com>
2021-02-04 00:32:00 +0300

    first commit

---
+++ budgetapp/src/context/AppReducer.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default (state, action) => {
+  switch (action.type) {
+    case 'ADD_INCOME':
+      return {
+        ...state,
+        incomeTransactions: [action.payload, ...state.incomeTransactions],
+      };
+    case 'ADD_EXPENSE':
+      return {
+        ...state,
+        expenseTransactions: [action.payload, ...state.expenseTransactions],
+      };
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        incomeTransactions: state.incomeTransactions.filter(
+          (incomeTransaction) => incomeTransaction.id !== action.payload
+        ),
+        expenseTransactions: state.expenseTransactions.filter(
+          (expenseTransaction) => expenseTransaction.id !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
